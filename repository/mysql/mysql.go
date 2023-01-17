4ca364023237bf4dfa8fa6f9438b724ac5762a4c
Jonas Gacrama <jonasgacrama@gmail.com>
2023-01-17 11:30:18 +0800

    main: Initial commit

---
+++ repository/mysql/mysql.go
@@ -0,0 +1,132 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	repo "github.com/moemoe89/go-unit-test-sql/repository"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Repository represent the Repository model
+type Repository struct {
+	DB *sql.DB
+}
+
+// NewRepository will create a variable that represent the Repository struct
+func NewRepository(dialect, dsn string, idleConn, maxConn int) (repo.Repository, error) {
+	db, err := sql.Open(dialect, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxIdleConns(idleConn)
+	db.SetMaxOpenConns(maxConn)
+
+	return &Repository{db}, nil
+}
+
+// Close attaches the provider and close the connection
+func (r *Repository) Close() {
+	r.db.Close()
+}
+
+// FindByID attaches the user repository and find data based on id
+func (r *Repository) FindByID(id string) (*repo.UserModel, error) {
+	user := new(repo.UserModel)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := r.db.QueryRowContext(ctx, "SELECT id, name, email, phone FROM users WHERE id = ?", id).Scan(&user.ID, &user.Name, &user.Email, &user.Phone)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+// Find attaches the user repository and find all data
+func (r *Repository) Find() ([]*repo.UserModel, error) {
+	users := make([]*repo.UserModel, 0)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	rows, err := r.db.QueryContext(ctx, "SELECT id, name, email, phone FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		user := new(repo.UserModel)
+		err = rows.Scan(
+			&user.ID,
+			&user.Name,
+			&user.Email,
+			&user.Phone,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+// Create attaches the user repository and creating the data
+func (r *Repository) Create(user *repo.UserModel) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	query := "INSERT INTO users (id, name, email, phone) VALUES (?, ?, ?, ?)"
+	stmt, err := r.db.PrepareContext(ctx, query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, user.ID, user.Name, user.Email, user.Phone)
+	return err
+}
+
+// Update attaches the user repository and update data based on id
+func (r *Repository) Update(user *repo.UserModel) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	query := "UPDATE users SET name = ?, email = ?, phone = ? WHERE id = ?"
+	stmt, err := r.db.PrepareContext(ctx, query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, user.Name, user.Email, user.Phone, user.ID)
+	return err
+}
+
+// Delete attaches the user repository and delete data based on id
+func (r *Repository) Delete(id string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	query := "DELETE FROM users WHERE id = ?"
+	stmt, err := r.db.PrepareContext(ctx, query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, id)
+	return err
+}
---
+++ repository/repository.go
@@ -0,0 +1,19 @@
+package repository
+
+// Repository represent the repositories
+type Repository interface {
+	Close()
+	FindByID(id string) (*UserModel, error)
+	Find() ([]*UserModel, error)
+	Create(user *UserModel) error
+	Update(user *UserModel) error
+	Delete(id string) error
+}
+
+// UserModel represent the user model
+type UserModel struct {
+	ID    string
+	Name  string
+	Email string
+	Phone string
+}
